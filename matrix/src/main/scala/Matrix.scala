c4867b9b6fecab677d47ee1e11f6dd34540e9af0
yu <ywang412@gmail.com>
2019-06-15 02:24:33 -0500

    w

--- matrix/src/main/scala/Matrix.scala
+++ matrix/src/main/scala/Matrix.scala
@@ -1,6 +1,8 @@
-object Matrix (val input: String){
-  val m = input.lines.split()
+case class Matrix (input: String){
+  val rows = input.lines.map(_.split(" ").map(_.toInt).toVector).toVector
+  val columns = rows.transpose
 
-
+  def row(r : Int) = rows(r)
+  def column(c : Int) = columns(c)
 
 }
